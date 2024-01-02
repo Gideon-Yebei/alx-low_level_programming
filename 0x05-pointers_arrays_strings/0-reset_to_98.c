70af717448aa1c372c46dedd39a428ddcc340190
Gideon-Yebei <0025.2022@students.mnu.ac.ke>
2024-01-02 08:55:32 -0800

    create 0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
  */
-
-void reset_to_98(int *n);
-int main(void)
+void reset_to_98(int *n)
 {
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
-void reset_to_98(int *n){
-    *n = 98;
+	*n = 98;
 }
