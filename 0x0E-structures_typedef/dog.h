c27f251eb048cae59bc7c1c5af5219ff80cd093f
Gideon-Yebei <0025.2022@students.mnu.ac.ke@gmail.com>
2024-01-23 18:09:46 +0300

    create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: Structure to store the details of a dog
+ */
+struct dog
+{
+		char *name;
+		float age;
+		char *owner;
+};
+
+#endif /* DOG_H */
